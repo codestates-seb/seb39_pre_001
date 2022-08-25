6fc4633c67a617b8499abb7c8c32f4663f2b155c
cleats01 <cleats01@gmail.com>
2022-08-25 13:29:38 +0900

    css 적용

--- client/src/components/Questions.jsx
+++ client/src/components/Questions.jsx
@@ -6,11 +6,67 @@ const StyledQuestions = styled.div`
   font-size: 16px;
   margin: auto;
   padding: 24px;
-  width: 750px;
-  border-left: 1px solid #666;
-  border-right: 1px solid #666;
+  width: 80%;
+  max-width: 750px;
+  border-left: 1px solid #d6d9dc;
+  border-right: 1px solid #d6d9dc;
+  border-bottom: 1px solid #d6d9dc;
   > .title {
     display: flex;
+    justify-content: space-between;
+    > h1 {
+      font-size: 27px;
+      font-weight: 500;
+      margin: 0 0 24px 0;
+    }
+    > button {
+      box-sizing: content-box;
+      color: #ffffff;
+      background-color: #0a95ff;
+      box-shadow: inset 0 1px 0 0 hsl(0deg 0% 100% / 40%);
+      padding: 10px;
+      border: 1px solid transparent;
+      border-radius: 3px;
+      font-size: 13px;
+      cursor: pointer;
+      width: 80px;
+      height: 15px;
+    }
+  }
+  > .sort {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 12px;
+    > .questions-counter {
+      font-size: 17px;
+    }
+    > .sort-container {
+      > button {
+        font-size: 12px;
+        color: #6a737c;
+        border: 1px solid #838c95;
+        padding: 10px;
+        background-color: #ffffff;
+        :first-child {
+          border-radius: 3px;
+          border-top-right-radius: 0;
+          border-bottom-right-radius: 0;
+        }
+        :not(:last-child) {
+          margin-right: -1px;
+        }
+        :last-child {
+          border-radius: 3px;
+          border-top-left-radius: 0;
+          border-bottom-left-radius: 0;
+        }
+        &.is-selected {
+          background-color: #e3e6e8;
+          color: #3b4045;
+        }
+      }
+    }
   }
 `;
 
@@ -22,10 +78,12 @@ function Questions() {
         <button>Ask Question</button>
       </div>
       <div className='sort'>
-        <span>{question.length} questions</span>
-        <button>최신순</button>
-        <button>좋아요순</button>
-        <button>싫어요순</button>
+        <div className='questions-counter'>{question.length} questions</div>
+        <div className='sort-container'>
+          <button className='is-selected'>최신순</button>
+          <button>좋아요순</button>
+          <button>싫어요순</button>
+        </div>
       </div>
       <div className='questions-container'>
         {question.map((e) => (
--- client/src/components/Summary.jsx
+++ client/src/components/Summary.jsx
@@ -2,18 +2,65 @@ import styled from 'styled-components';
 
 const StyledSummary = styled.div`
   display: flex;
-  border-top: 1px solid #888;
-  padding: 10px;
+  border-top: 1px solid #d6d9dc;
+  padding: 16px;
+  padding-right: 24px;
+  margin: 0 -24px 0 -24px;
   > .stats {
-    width: 100px;
+    display: flex;
+    flex-direction: column;
+    flex-shrink: 0;
+    flex-wrap: wrap;
+    align-items: flex-end;
+    width: 108px;
     text-align: right;
     margin: 0 16px 4px 0;
-    > div {
-      margin-bottom: 10px;
+    gap: 6px;
+    font-size: 13px;
+    > .view {
+      color: #6a737c;
     }
   }
   > .content {
-    width: 400px;
+    width: 600px;
+    > .title {
+      font-weight: 400;
+      padding-right: 24px;
+      margin: -2px 0 5px 0;
+      line-height: calc(17 / 13);
+      > a {
+        text-decoration: none;
+        font-size: 17px;
+      }
+    }
+    > .body {
+      font-size: 13px;
+      margin: -2px 0 8px 0;
+    }
+    > .meta {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      flex-wrap: wrap;
+      > .tags {
+        display: flex;
+        flex-wrap: wrap;
+        width: 80%;
+        gap: 4px;
+        > div {
+          font-size: 12px;
+          color: #39739d;
+          background-color: #e1ecf4;
+          margin: 0 2px 2px 0;
+          padding: 4.8px 6px;
+          border-radius: 4px;
+        }
+      }
+      > .user {
+        margin-left: auto;
+        font-size: 12px;
+      }
+    }
   }
 `;
 
@@ -27,14 +74,16 @@ function Summary({ question }) {
         <div className='view'>{view} views</div>
       </div>
       <div className='content'>
-        <a className='title' href='https://stackoverflow.com/questions/73480702/maximum-concurrent-dpu-usage-count'>
-          {title}
-        </a>
+        <h3 className='title'>
+          <a className='title' href='https://stackoverflow.com/questions/73480702/maximum-concurrent-dpu-usage-count'>
+            {title}
+          </a>
+        </h3>
         <div className='body'>{body}</div>
         <div className='meta'>
           <div className='tags'>
-            {tags.map((e, i) => (
-              <div key={i}>{e}</div>
+            {tags.map((tag, i) => (
+              <div key={i}>{tag}</div>
             ))}
           </div>
           <div className='user'>
