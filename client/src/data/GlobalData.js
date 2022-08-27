2ef0681d3c94db78769a7105dea051f127a721fc
dubipy <qnfrha999@naver.com>
2022-08-28 08:00:35 +0900

    1차 테스트

--- client/src/data/GlobalData.js
+++
@@ -1,3 +0,0 @@
-export const websiteName = 'Ezent';
-export const light = 'white';
-export const dark = '#020305';
--- client/src/pages/SignupPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Form from '../components/Form/Form';
-
-const SignupPage = () => {
-	return <Form />;
-};
-
-export default SignupPage;
