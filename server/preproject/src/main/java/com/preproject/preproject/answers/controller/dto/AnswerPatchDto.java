bab52b6f20d944382c543377bf27d5b1b2d39367
Hyeonsu Bang / Thom <79960964+thom-droid@users.noreply.github.com>
2022-08-27 15:40:31 +0900

    Back/feat/beef 45 (#6)
    * Api Controller
    
    * Answers, Tags, Users API Controller
    
    * Back/feat/beef 36 (#2)
    
    * gitignore
    
    * gitignore
    
    * chore : entities
    
    * chore : wokring on stubbing..
    
    * feat : entities and handler method stubbed
    
    - Add handler methods (/questions/) with stubbed data in QuestionController.
    - Add DTOs and Mapper for Question entity.
    - Add QuestionStubService for stubbing. To be replaced.
    - Add IndexController for common apis (/abouts, /forTeams, etc..).
    
    * gitignore
    
    * feat : add like or dislike handler method
    
    - Add like/dislike handler methods that return simple string.
    
    Co-authored-by: thom <thom-droid@github.com>
    
    Co-authored-by: aprochoi <ym24319577@gmail.com>
    Co-authored-by: thom <thom-droid@github.com>

---
+++ server/preproject/src/main/java/com/preproject/preproject/answers/controller/dto/AnswerPatchDto.java
@@ -0,0 +1,11 @@
+package com.preproject.preproject.answers.controller.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AnswerPatchDto {
+    private long answer_id;
+    private String content;
+}
---
+++ server/preproject/src/main/java/com/preproject/preproject/answers/controller/dto/AnswersPostDto.java
@@ -0,0 +1,8 @@
+package com.preproject.preproject.answers.controller.dto;
+
+import lombok.Getter;
+
+@Getter
+public class AnswersPostDto {
+    private String content;
+}
