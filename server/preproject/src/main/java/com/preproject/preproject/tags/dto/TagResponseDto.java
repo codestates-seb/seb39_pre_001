b52c3d36a01b0399b2579f1558863bf9ec96b14b
Hyeonsu Bang / Thom <79960964+thom-droid@users.noreply.github.com>
2022-08-29 15:11:11 +0900

    Back/feat/beef 44 (#7)
    * API 작성 전 build.gradle 설정
    
    * User Controller Test API
    
    * Answer, Tags, Users API RestDocs Complete
    
    * build for API index.html
    
    Co-authored-by: aprochoi <ym24319577@gmail.com>

---
+++ server/preproject/src/main/java/com/preproject/preproject/tags/dto/TagResponseDto.java
@@ -0,0 +1,11 @@
+package com.preproject.preproject.tags.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TagResponseDto {
+    private long tag_id;
+    private String name;
+}
