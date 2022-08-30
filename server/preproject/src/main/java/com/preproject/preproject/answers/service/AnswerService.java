6ab0c9cec8ae94f1bf31000269b10d8a93859362
aprochoi <ym24319577@gmail.com>
2022-08-30 18:39:15 +0900

    Answer Service

--- server/preproject/src/main/java/com/preproject/preproject/answers/service/AnswerService.java
+++ server/preproject/src/main/java/com/preproject/preproject/answers/service/AnswerService.java
@@ -1,22 +1,49 @@
 package com.preproject.preproject.answers.service;
 
 import com.preproject.preproject.answers.entity.Answer;
+import com.preproject.preproject.answers.repository.AnswerRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AnswerService {
 
+    private final AnswerRepository answerRepository;
+
+    public AnswerService(AnswerRepository answerRepository) {
+        this.answerRepository = answerRepository;
+    }
+
     public Answer createAnswer(Answer answer) {
 
-        return answer;
+        return answerRepository.save(answer);
     }
 
     public Answer updateAnswer(Answer answer) {
 
-        return answer;
+        Answer findAnswer = findVerifiedAnswer(answer.getAnswerId());
+
+        Optional.ofNullable(answer.getContent()).ifPresent(content -> findAnswer.setContent(content));
+
+        return answerRepository.save(findAnswer);
     }
 
     public void deleteAnswer(long answerId) {
+        Answer findAnswer = findVerifiedAnswer(answerId);
 
+        answerRepository.delete(findAnswer);
+
+    }
+
+    public Answer findVerifiedAnswer(long answerId) {
+        Optional<Answer> optionalAnswer =
+                answerRepository.findById(answerId);
+
+        Answer findAnswer = optionalAnswer.orElseThrow(() ->
+                new RuntimeException(String.valueOf(HttpStatus.NOT_FOUND)));
+
+        return findAnswer;
     }
 }
