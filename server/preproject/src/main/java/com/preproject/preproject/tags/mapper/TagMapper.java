f07dcd5a807fc984264189f1713fff91b05fb7bf
thom-window <hsbang.thom@gmail.com>
2022-09-01 02:30:03 +0900

    merge and remove some ambiguous files.

--- server/preproject/src/main/java/com/preproject/preproject/tags/mapper/TagMapper.java
+++ server/preproject/src/main/java/com/preproject/preproject/tags/mapper/TagMapper.java
@@ -4,15 +4,15 @@ import com.preproject.preproject.tags.dto.TagResponseDto;
 import com.preproject.preproject.tags.entity.Tag;
 import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
-
-@Component
-@Mapper(componentModel = "spring")
-public class TagMapper {
-
-    public TagResponseDto tagResponse(Tag tags) {
-        long tagId = tags.getTagId();
-        String name = tags.getName();
-
-        return new TagResponseDto(tagId, name);
-    }
-}
+//
+//@Component
+//@Mapper(componentModel = "spring")
+//public class TagMapper {
+//
+//    public TagResponseDto tagResponse(Tag tags) {
+//        long tagId = tags.getTagId();
+//        String name = tags.getName();
+//
+//        return new TagResponseDto(tagId, name);
+//    }
+//}
