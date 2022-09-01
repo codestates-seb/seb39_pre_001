ad9cdd2e9da5c2bd1f6e22b0b29f5769eeb653d3
aprochoi <ym24319577@gmail.com>
2022-09-01 18:12:56 +0900

    login feature

--- server/preproject/src/main/java/com/preproject/preproject/users/controller/UsersController.java
+++ server/preproject/src/main/java/com/preproject/preproject/users/controller/UsersController.java
@@ -1,16 +1,19 @@
 package com.preproject.preproject.users.controller;
 
 
+import com.preproject.preproject.users.dto.UserLoginDto;
 import com.preproject.preproject.users.dto.UsersPostDto;
 import com.preproject.preproject.users.entity.Users;
 import com.preproject.preproject.users.mapper.UserMapper;
 import com.preproject.preproject.users.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UsersController {
 
     private final UserService userService;
@@ -35,5 +38,17 @@ public class UsersController {
     public ResponseEntity getUser() {
 
         return new ResponseEntity<>("Welcome Join User", HttpStatus.OK);
+    }
+
+    //login 기능
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody UserLoginDto loginDto) {
+        log.info("email = {} , password = {} ", loginDto.getEmail(), loginDto.getPassword());
+
+        if (userService.login(loginDto).equals("Success")) {
+            return new ResponseEntity<>("로그인 성공", HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>("로그인 실패", HttpStatus.BAD_REQUEST);
     }
 }
--- server/preproject/src/main/java/com/preproject/preproject/users/service/UserService.java
+++ server/preproject/src/main/java/com/preproject/preproject/users/service/UserService.java
@@ -1,13 +1,17 @@
 package com.preproject.preproject.users.service;
 
+import com.preproject.preproject.users.dto.UserLoginDto;
 import com.preproject.preproject.users.entity.Users;
 import com.preproject.preproject.users.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class UserService {
 
     private final UserRepository userRepository;
@@ -29,5 +33,22 @@ public class UserService {
                 new RuntimeException("유저가 없습니다."));
 
         return findUser;
+    }
+
+
+    public String login(UserLoginDto userLoginDto) {
+
+        Optional<Users> user = userRepository.findByEmail(userLoginDto.getEmail());
+
+        user.orElseThrow(() -> new NoSuchElementException("존재하는 이메일이 없습니다."));
+
+        log.info("db password : {} , input password : {} ", user.get().getPassword(), userLoginDto.getPassword());
+
+        if (user.get().getPassword().equals(userLoginDto.getPassword())) {
+            return "Success";
+        }
+
+        return "Failed";
+
     }
 }
